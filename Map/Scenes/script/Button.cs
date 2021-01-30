e06e9bf31db6fe81e4733a15e2c82ce57af99426
mathis-la-debrouille <72014586+mathis-la-debrouille@users.noreply.github.com>
2021-01-30 23:50:07 +0100

    bite

---
+++ Map/Scenes/script/Button.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    [SerializeField] private Opening door;
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.E))
+            door.doorOpen();
+        if (Input.GetKeyDown(KeyCode.G))
+            door.doorClose();
+    }
+}
