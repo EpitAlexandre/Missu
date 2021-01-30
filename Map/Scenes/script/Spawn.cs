7ab98659537ea84fb1118f295e1604fb8743713e
alexandre collin <alexandre.et.collin@gmail.com>
2021-01-31 00:31:35 +0100

    clean repo

--- Map/Scenes/script/Spawn.cs
+++
@@ -1,9 +0,0 @@
-﻿/*using UnityEngine;
-
-public class PlayerSpawn : MonoBehaviour
-{
-    private void Awake();
-    {
-        GameObject.FindGameObjectWithTag("dino").transform.position = transform.position;
-    }
-}*/
