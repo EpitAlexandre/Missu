6ff3df29003599ec4c138d5906cb31d0e9438a1b
alexandre collin <alexandre.et.collin@gmail.com>
2021-01-31 14:29:13 +0100

    Create first map

---
+++ Assets/Assets Map/Assets Map/Platform/Interactable.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interactable : MonoBehaviour
+{
+    public bool isInRang;
+    public KeyCode interactKey1;
+    public KeyCode interactKey2;
+    public UnityEvent interactAction;
+
+    void Update()
+    {
+        if (isInRang)
+        {
+            if (Input.GetKeyDown(interactKey1) || Input.GetKeyDown(interactKey2))
+            {
+                interactAction.Invoke();
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Dino1") || collision.gameObject.CompareTag("Dino2"))
+        {
+            isInRang = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Dino1") || collision.gameObject.CompareTag("Dino2"))
+        {
+            isInRang = false;
+        }
+    }
+}
