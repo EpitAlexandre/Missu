22de11e6a851321988430544bf8804464970e1c4
alexandre collin <alexandre.et.collin@gmail.com>
2021-01-31 04:58:37 +0100

    add pallette

---
+++ Assets/Assets Map/Platform/Platform.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    public Transform pos1;
+    public Transform pos2;
+    public float speed;
+    public Transform startPos;
+    public bool is_activate;
+
+    Vector3 nextPos;
+
+    void Start()
+    {
+        nextPos = startPos.position;
+    }
+
+    void FixedUpdate()
+    {
+        if (transform.position.x == pos1.position.x)
+        {
+            nextPos = pos2.position;
+        }
+        if (transform.position.x == pos2.position.x)
+        {
+            nextPos = pos1.position;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, nextPos, speed * Time.deltaTime);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(pos1.position, pos2.position);
+    }
+
+}
